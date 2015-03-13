7485f28e862429655ca2ab7815be482bf79e5b32
Olivier Piras <qb50@oprs.eu>
2015-03-13 11:09:58 +0100

    Em::Blocks - ajout des targets ODB et ADCS

---
+++ sat/ADCS/main.cpp
@@ -0,0 +1,38 @@
+
+#include "qb50.h"
+#include <stdio.h>
+
+using namespace qb50;
+
+
+void thread1( Thread *self )
+{
+   int cnt = 0;
+
+   (void)self;
+
+   for( ;; ) {
+      (void)printf( "hello: %p - %d\r\n", self, cnt );
+      ++cnt;
+      delay( 500 );
+   }
+}
+
+
+int main( void )
+{
+   /* enable UART6 (debug) */
+
+   UART6.enable();
+
+   /* create worker threads */
+
+   createThread( "Thread 1", thread1 );
+
+   run();
+
+   /* never reached */
+   for( ;; );
+}
+
+/*EoF*/
