56f65739933207640a8509c8278f2d0972367711
Olivier Piras <qb50@oprs.eu>
2015-09-15 22:13:18 +0200

    Make sure the FPU is enabled in SystemInit (kudos to clive1 - https://goo.gl/uXaSV0)

--- sat/sys/stm32f4/src/system_stm32f4xx.c
+++ sat/sys/stm32f4/src/system_stm32f4xx.c
@@ -24,11 +24,9 @@ __I uint8_t  AHBPrescTable[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 
 
 void SystemInit( void )
 {
-  return;  /* for now... */
+  SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
 
-  #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
-    SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
-  #endif
+  return;  /* for now... */
 
   /* Reset the RCC clock configuration to the default reset state */
 
