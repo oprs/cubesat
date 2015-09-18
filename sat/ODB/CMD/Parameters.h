1c6e9e3b2ac770baeff96d56da60c3a12806d605
Olivier Piras <qb50@oprs.eu>
2015-09-18 12:47:13 +0200

    added CMD thread (including qb50::Parameters & qb50::FormParser)

---
+++ sat/ODB/CMD/Parameters.h
@@ -0,0 +1,35 @@
+
+#ifndef _QB50_PARAMETERS_H
+#define _QB50_PARAMETERS_H
+
+#define _QB50_NPARAMS 15
+
+
+namespace qb50 {
+
+   class Parameters
+   {
+
+      public:
+
+         Parameters();
+         virtual ~Parameters();
+
+         void set( int pnum, long pval );
+         long get( int pnum );
+
+
+      private:
+
+         long pv[ _QB50_NPARAMS ];
+
+   };
+
+    extern qb50::Parameters PARAMS;
+
+}
+
+
+#endif /*_QB50_PARAMETERS_H*/
+
+/*EoF*/
