617fa4c9a9957039b982bf30bbf85efaa8500e5e
Olivier Piras <qb50@oprs.eu>
2015-09-28 02:42:13 +0200

    added a hard fault handler for debugging

--- sat/sys/src/system/Application.cpp
+++ sat/sys/src/system/Application.cpp
@@ -1,6 +1,7 @@
 
 #include <FreeRTOS.h>
 #include <task.h>
+#include <cstdio> // XXX
 
 #include "system/Application.h"
 
@@ -14,6 +15,8 @@
  void vApplicationIdleHook( void );
  void vApplicationMallocFailedHook( void );
  void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+ void HardFault_Handler( void ) __attribute__(( naked ));
+ void hard_fault_handler_c( uint32_t *argv );
 
  #ifdef __cplusplus
   }
@@ -34,6 +37,72 @@
     (void)pcTaskName;
 
     for( ;; );
+ }
+
+/*
+ void HardFault_Handler( void )
+ { for( ;; ); }
+*/
+
+// From Joseph Yiu, minor edits by FVH
+// hard fault handler in C,
+// with stack frame location as input parameter
+// called from HardFault_Handler in file xxx.s
+ void hard_fault_handler_c( uint32_t *argv )
+ {
+  volatile uint32_t r0;
+  volatile uint32_t r1;
+  volatile uint32_t r2;
+  volatile uint32_t r3;
+  volatile uint32_t r12;
+  volatile uint32_t r14;
+  volatile uint32_t r15;
+  volatile uint32_t psr;
+
+  r0  = argv[0];
+  r1  = argv[1];
+  r2  = argv[2];
+  r3  = argv[3];
+  r12 = argv[4];
+  r14 = argv[5];
+  r15 = argv[6];
+  psr = argv[7];
+
+  printf( "\r\n\r\n*** HARD FAULT HANDLER ***\r\n\r\n" );
+
+  printf( "   R0: 0x%08lx\r\n", r0 );
+  printf( "   R1: 0x%08lx\r\n", r1 );
+  printf( "   R2: 0x%08lx\r\n", r2 );
+  printf( "   R3: 0x%08lx\r\n", r3 );
+  printf( "  R12: 0x%08lx\r\n", r12 );
+  printf( "  R14: 0x%08lx (LR - return address)\r\n", r14 );
+  printf( "  R15: 0x%08lx (PC - program counter)\r\n", r15 );
+  printf( "  PSR: 0x%08lx\r\n", psr );
+  printf( " BFAR: 0x%08lx\r\n", (*((volatile unsigned long *)(0xe000ed38))));
+  printf( " CFSR: 0x%08lx\r\n", (*((volatile unsigned long *)(0xe000ed28))));
+  printf( " HFSR: 0x%08lx\r\n", (*((volatile unsigned long *)(0xe000ed2c))));
+  printf( " DFSR: 0x%08lx\r\n", (*((volatile unsigned long *)(0xe000ed30))));
+  printf( " AFSR: 0x%08lx\r\n", (*((volatile unsigned long *)(0xe000ed3c))));
+//printf( " SCB_SHCSR = %x\r\n", SCB->SHCSR );
+
+  printf( "\r\nSYSTEM HALTED\r\n" );
+
+  for( ;; );
+ }
+
+ void HardFault_Handler( void )
+ {
+    __asm volatile
+    (
+        " tst lr, #4                                         \n"
+        " ite eq                                             \n"
+        " mrseq r0, msp                                      \n"
+        " mrsne r0, psp                                      \n"
+        " ldr r1, [r0, #24]                                  \n"
+        " ldr r2, handler2_address_const                     \n"
+        " bx r2                                              \n"
+        " handler2_address_const: .word hard_fault_handler_c \n"
+    );
  }
 #endif /*DEBUG*/
 
@@ -71,9 +140,6 @@ Thread* registerThread( Thread *thread )
 
    if( rv != pdTRUE )
       { throw 42; /* XXX */ }
-
-   /* initial state: suspended */
-   vTaskSuspend( thread->handle );
 
    return thread;
 }
